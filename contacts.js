036d41d0a653effefd62f796ac3fada1fb0c2073
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-05-08 15:25:09 +0300

    add basic files

---
+++ contacts.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const contactsPath = './contacts.js';
+
+// TODO: задокументувати кожну функцію
+
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
